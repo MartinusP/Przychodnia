5633fb42cc55b2bef4e549d773e35e32d9cfb84f
Marcin Popiolek <popiolek1@interia.pl>
2017-06-12 19:31:34 +0200

    Model Pacjent

--- Przychodnia/Models/PacjentModel.cs
+++ Przychodnia/Models/PacjentModel.cs
@@ -7,9 +7,13 @@ namespace Przychodnia.Models
 {
     public class PacjentModel
     {
-        public int ID_Pracjent { get; set; }
+        public int ID_Pacjent { get; set; }
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
+        public string Adres { get; set; }
+        public string Pesel { get; set; }
+        public string Telefon { get; set; }
+        public string Email { get; set; }
 
     }
 }
