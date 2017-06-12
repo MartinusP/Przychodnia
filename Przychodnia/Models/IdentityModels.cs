64eef4cdad92eb9bab972c5cad356f8ec97c7944
Marcin Popiolek <popiolek1@interia.pl>
2017-06-13 01:31:02 +0200

    connectrionString do Identity

--- Przychodnia/Models/IdentityModels.cs
+++ Przychodnia/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Przychodnia.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("PRZYCHODNIAAutoryzacja", throwIfV1Schema: false)
         {
         }
 
