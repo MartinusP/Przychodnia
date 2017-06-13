f864626f120e287c6e64f974f1d0b0494b947d9a
Marcin Popiolek <popiolek1@interia.pl>
2017-06-13 18:53:37 +0200

    poprawki w mechanizmie autoryzacji i nowy bootstrap 3.3.7

--- Przychodnia/Controllers/HomeController.cs
+++ Przychodnia/Controllers/HomeController.cs
@@ -1,26 +1,18 @@
-﻿using Przychodnia.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Przychodnia.Controllers
+namespace IdentitySample.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            using (PRZYCHODNIAEntities db = new PRZYCHODNIAEntities()) ;
-            {
-
-            }
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your app description page.";
 
             return View();
         }
--- Przychodnia/Models/IdentityModels.cs
+++ Przychodnia/Models/IdentityModels.cs
@@ -1,10 +1,10 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace Przychodnia.Models
+namespace IdentitySample.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
@@ -21,8 +21,15 @@ namespace Przychodnia.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("PRZYCHODNIAAutoryzacja", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
         }
 
         public static ApplicationDbContext Create()
