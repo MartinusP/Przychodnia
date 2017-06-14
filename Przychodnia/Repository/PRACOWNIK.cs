3531121c1d6c26bdaeb7218e34a2fff82248e986
Marcin Popiolek <popiolek1@interia.pl>
2017-06-14 14:42:43 +0200

    zmiany w nazwenictwie Modelu

--- Przychodnia/Repository/PRACOWNIK.cs
+++ Przychodnia/Repository/PRACOWNIK.cs
@@ -11,25 +11,20 @@ namespace Przychodnia.Repository
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-
-    [Table("PRACOWNIK")]
+    
     public partial class PRACOWNIK
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public PRACOWNIK()
         {
             this.ODDZIAL_PRACOWNIK = new HashSet<ODDZIAL_PRACOWNIK>();
-            this.WIZYTAs = new HashSet<WIZYTA>();
+            this.WIZYTY = new HashSet<WIZYTA>();
         }
-
-        [Key]
+    
         public int ID_PRACOWNIK { get; set; }
         public string IMIE { get; set; }
         public string NAZWISKO { get; set; }
         public string ADRES { get; set; }
-        [EmailAddress]
         public string EMAIL_KONTAKTOWY { get; set; }
         public Nullable<int> ID_SPECJALIZACJA { get; set; }
         public Nullable<int> ID_ODDZIAL_PRACOWNIK { get; set; }
@@ -40,6 +35,6 @@ namespace Przychodnia.Repository
         public virtual DYZUR DYZUR { get; set; }
         public virtual SPECJALIZACJA SPECJALIZACJA { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<WIZYTA> WIZYTAs { get; set; }
+        public virtual ICollection<WIZYTA> WIZYTY { get; set; }
     }
 }
