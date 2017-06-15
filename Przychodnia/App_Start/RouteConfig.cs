50e1d63b806bb433036c2fbf1b6c0724b6359a24
Marcin Popiolek <popiolek1@interia.pl>
2017-06-15 02:52:51 +0200

    dodanie dostęów do ról

--- Przychodnia/App_Start/RouteConfig.cs
+++ Przychodnia/App_Start/RouteConfig.cs
@@ -14,6 +14,66 @@ namespace IdentitySample
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+               "Home",
+               "Home/",
+               new { controller = "Home", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Dyzur",
+               "Dyzur/",
+               new { controller = "Dyzur", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Oddzial",
+               "Oddzial/",
+               new { controller = "Oddzial", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Pacjent",
+               "Pacjent/",
+               new { controller = "Pacjent", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Placowka",
+               "Placowka/",
+               new { controller = "Placowka", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Pracownik",
+               "Pracownik/",
+               new { controller = "Pracownik", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Recepta",
+               "Recepta/",
+               new { controller = "Recepta", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Sala",
+               "Sala/",
+               new { controller = "Sala", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Specjalizacja",
+               "Specjalizacja/",
+               new { controller = "Specjalizacja", action = "Index" }
+           );
+
+            routes.MapRoute(
+               "Wizyta",
+               "Wizyta/",
+               new { controller = "Wizyta", action = "Index" }
+           );
         }
     }
 }
